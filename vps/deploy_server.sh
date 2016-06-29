5e307313e476c0e5532732401100a19a84bd06e4
Chao Wang <chaowang.thu@gmail.com>
2016-06-29 14:55:29 +0800

    Added server scripts.

---
+++ vps/deploy_server.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y install htop
+sudo apt-get -y install squid
+sudo apt-get -y install supervisor
+
+curl -L https://raw.githubusercontent.com/CharlesWong/darkpassenger/master/vps/squid.conf > tmp.conf
+sudo cat tmp.conf >> /etc/squid3/squid.conf
+sudo cat tmp.conf >> /etc/squid/squid.conf
+sudo service squid restart
+
+wget -O dp https://github.com/CharlesWong/darkpassenger/raw/master/vps/dp
+cp ./dp /usr/bin/
+sudo chmod +x /usr/bin/dp
+
+curl -L https://raw.githubusercontent.com/CharlesWong/darkpassenger/master/vps/supervisord.conf > tmp.conf
+sudo cat tmp.conf >> /etc/supervisor/supervisord.conf
+
+#sudo echo '#!/bin/sh -e' > /etc/rc.local
+#sudo echo '/usr/bin/dp -listen=:9156 -secret=qwertyuiop123 -crypto=rc4 -backend=127.0.0.1:8888 -clientmode=false &' >> /etc/rc.local
+
+sudo reboot
